ec437b1d6b9444214a51d498fd07cd5c01a32765
Mavis Davis <mavisledavis563@gmail.com>
2026-04-16 10:49:14 -0500

    Refactor makeTxt function to handle textIndex properly
    Fix syntax error in makeTxt function and ensure textIndex does not exceed textList length.

--- Scripts/addText.js
+++ Scripts/addText.js
@@ -3,21 +3,23 @@ textList = ["test1", "test2"];
 textIndex = 0; 
 const parentElementList = [$("#header")[0]];  
 var delay = 0; 
-function makeTxt() {   
-    $("#text-box").text(""); 
-    var element = $("#text-box");    
-    console.log(element); 
-    var repeat = false;  
-    var text = textList[textIndex]; 
-    var i = 0;   
-    var textInt = setInterval(function() { 
-        element.append(text[i]);  
+function makeTxt() {    
+    if (!(textIndex > textList.length - 1) {  
+        $("#text-box").text(""); 
+        var element = $("#text-box");    
         console.log(element); 
-        i++; 
-        if (element.text().length == text.length ||  text.length == 0) { 
-            clearInterval(textInt);  
-        } 
-    }, 30);  
-    textIndex++;     
+        var repeat = false;  
+        var text = textList[textIndex]; 
+        var i = 0;   
+        var textInt = setInterval(function() { 
+            element.append(text[i]);  
+            console.log(element); 
+            i++; 
+            if (element.text().length == text.length ||  text.length == 0) { 
+                clearInterval(textInt);  
+            } 
+        }, 30);  
+        textIndex++;   
+    }
 }    
 
