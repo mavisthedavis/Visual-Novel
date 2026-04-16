0ea138fb17fa97b09611d393768916a1260ae374
Mavis Davis <mavisledavis563@gmail.com>
2026-04-16 12:14:05 -0500

    Fix image source assignment in imageChange function

--- Scripts/addImage.js
+++ Scripts/addImage.js
@@ -19,5 +19,5 @@ function imageChange() {
     if (imgList[textIndex].effect != false) { 
       console.log("test"); 
     } 
-    $($("img")[0]).src = imgList[textIndex].src; 
+    $($("img")[0]).attr("src", imgList[textIndex].src); 
 } 
