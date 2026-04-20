e4dfc121463b727a747a96992896320402b6aa87
Mavis Davis <mavisledavis563@gmail.com>
2026-04-20 18:08:44 -0500

    Refactor imageChange and fix transCheck condition
    Refactor imageChange function to include transCheck call and fix assignment in transCheck condition.

--- Scripts/addImage.js
+++ Scripts/addImage.js
@@ -1,8 +1,20 @@
 
 
-function imageChange() { 
+function imageChange() {  
+    transCheck(); 
     if (imgList[textIndex].effect != false) { 
       console.log("test"); 
     } 
     $($("img")[0]).attr("src", imgList[textIndex].src); 
-} 
+}  
+
+function transCheck() {  
+    if (textIndex = 7) { 
+        $("#text-container").css("background-color", "blue");
+        $("#text-box").css("background-color", "blue");   
+        $("#ext").css("background-color", "blue");  
+        $("#view").css("background-color", "white");  
+        $("#text").css("color", "white"); 
+    }
+
+}
