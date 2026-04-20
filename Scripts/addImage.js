deb1a554064a04df2e51f6c473feff46fdfbd529
Mavis Davis <mavisledavis563@gmail.com>
2026-04-20 09:30:30 -0500

    Remove image class and instances from addImage.js
    Removed the image class and its instances from the script.

--- Scripts/addImage.js
+++ Scripts/addImage.js
@@ -1,20 +1,5 @@
-class image {  
-  #src; 
-  #effect; 
-  constructor (src, effect) { 
-    this.#src = src; 
-    this.#effect = effect; 
-  } 
 
-  get src() { 
-    return this.#src;  
-  } 
-  get effect() { 
-    return this.#effect; 
-  }
-}  
 
-var imgList = [new image("Images/Even edit viboe.jpg", false), new image("Images/Even edit viboe.jpg", false)];  
 function imageChange() { 
     if (imgList[textIndex].effect != false) { 
       console.log("test"); 
