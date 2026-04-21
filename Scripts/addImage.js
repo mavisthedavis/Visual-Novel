ae62adec704cda58e466e91e967fd7b935315350
Mavis Davis <mavisledavis563@gmail.com>
2026-04-21 10:05:04 -0500

    Update audio sources and playback in addImage.js

--- Scripts/addImage.js
+++ Scripts/addImage.js
@@ -15,28 +15,34 @@ function transCheck() {
         $("#text-box").css("background-color", "white");   
         $(".ext").css("background-color", "#748584");  
         $("#view").css("background-color", "white");  
-        $("#text").css("color", "black"); 
+        $("#text").css("color", "black");  
+        $("#audio").attr("src", "Songs/School.mp3");  
+        $("#audio")[0].play(); 
     } 
     if (textIndex == 39) { 
         $("#text-container").css("background-color", "#190c00");
         $("#text-box").css("background-color", "#e4d9ce");   
         $(".ext").css("background-color", "#190c00");  
         $("#view").css("background-color", "#e4d9ce");  
-        $("#text").css("color", "black"); 
+        $("#text").css("color", "black");  
+        $("#audio").attr("src", "Songs/Parents song.mp3"); 
     } 
     if (textIndex == 58) { 
         $("#text-container").css("background-color", "#036b16");
         $("#text-box").css("background-color", "white");   
         $(".ext").css("background-color", "#036b16");  
         $("#view").css("background-color", "white");  
-        $("#text").css("color", "black"); 
+        $("#text").css("color", "black");  
+        $("#audio").attr("src", "Songs/feild.mp3"); 
     } 
     if (textIndex == 108) { 
         $("#text-container").css("background-color", "black");
         $("#text-box").css("background-color", "black");   
         $(".ext").css("background-color", "black");  
         $("#view").css("background-color", "black");  
-        $("#text").css("color", "white"); 
+        $("#text").css("color", "white");  
+        $("#audio")[0].pause(); 
+
     }
 
 }
